2a013607d8242706c85c3587ee32c38e0794381a
Jose Loarca <joseloarca97@icloud.com>
2026-02-18 14:52:00 -0500

    Initial commit

---
+++ test/cdk-pizza.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPizza from '../lib/cdk-pizza-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pizza-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPizza.CdkPizzaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
