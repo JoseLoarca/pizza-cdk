c389c8637566500a7f2b64bad16247a37d20cc2f
Jose Loarca <joseloarca97@icloud.com>
2026-02-19 15:19:43 -0500

    Added code for SQS queue

--- lib/cdk-pizza-stack.ts
+++ lib/cdk-pizza-stack.ts
@@ -2,7 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Function } from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class CdkPizzaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,13 +23,25 @@ export class CdkPizzaStack extends cdk.Stack {
       code: Code.fromAsset('lib/functions'),
     });
 
+    const prepOrderFunction = new Function(this, 'PrepOrderFunction', {
+      runtime: Runtime.NODEJS_24_X,
+      handler: 'handler.prepOrder',
+      code: Code.fromAsset('lib/functions')
+    });
+
     // API Gateway
-    const api = new apigateway.RestApi(this, 'PizzaApi', {
+    const api = new RestApi(this, 'PizzaApi', {
       restApiName: 'Pizza CDK Service'
     });
 
     const orderResource = api.root.addResource('order');
-    orderResource.addMethod('POST', new apigateway.LambdaIntegration(newOrderFunction));
-    orderResource.addResource('{orderId}').addMethod('GET', new apigateway.LambdaIntegration(getOrderFunction));
+    orderResource.addMethod('POST', new LambdaIntegration(newOrderFunction));
+    orderResource.addResource('{orderId}').addMethod('GET', new LambdaIntegration(getOrderFunction));
+
+    // SQS
+    const pendingOrdersQueue = new Queue(this, 'PendingOrdersQueue', {});
+    prepOrderFunction.addEventSource(new SqsEventSource(pendingOrdersQueue, {
+      batchSize: 1
+    }));
   }
 }
