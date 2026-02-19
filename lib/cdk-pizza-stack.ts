7cc28eccca7bcad12fa65109fdb94b234679c4c4
Jose Loarca <joseloarca97@icloud.com>
2026-02-19 12:45:19 -0500

    Added API Gateway definition and Lambda code

--- lib/cdk-pizza-stack.ts
+++ lib/cdk-pizza-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Function } from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkPizzaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Functions
+    const newOrderFunction = new Function(this, 'NewOrderFunction', {
+      runtime: Runtime.NODEJS_24_X,
+      handler: 'handler.newOrder',
+      code: Code.fromAsset('lib/functions'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPizzaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getOrderFunction = new Function(this, 'GetOrderFunction', {
+      runtime: Runtime.NODEJS_24_X,
+      handler: 'handler.getOrder',
+      code: Code.fromAsset('lib/functions'),
+    });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'PizzaApi', {
+      restApiName: 'Pizza CDK Service'
+    });
+
+    const orderResource = api.root.addResource('order');
+    orderResource.addMethod('POST', new apigateway.LambdaIntegration(newOrderFunction));
+    orderResource.addResource('{orderId}').addMethod('GET', new apigateway.LambdaIntegration(getOrderFunction));
   }
 }
