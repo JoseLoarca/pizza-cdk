6999b661a96d47d19d7320b9a2ea38bf3b25ebe4
Jose Loarca <joseloarca97@icloud.com>
2026-02-20 17:03:32 -0500

    Added connection to DynamoDB table

--- lib/cdk-pizza-stack.ts
+++ lib/cdk-pizza-stack.ts
@@ -1,67 +1,73 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Function } from 'aws-cdk-lib/aws-lambda';
-import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { Queue } from 'aws-cdk-lib/aws-sqs';
-import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import {Construct} from 'constructs';
+import {Code, Function, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
+import {Queue} from 'aws-cdk-lib/aws-sqs';
+import {SqsEventSource} from 'aws-cdk-lib/aws-lambda-event-sources';
+import {AttributeType, BillingMode, Table} from "aws-cdk-lib/aws-dynamodb";
 
 export class CdkPizzaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // SQS
-    const pendingOrdersQueue = new Queue(this, 'PendingOrdersQueue', {});
-    const ordersToSendQueue = new Queue(this, 'OrdersToSendQueue', {});
+        // Dynamo DB
+        const ordersTable = new Table(this, 'OrdersTable', {
+            partitionKey: {name: 'orderId', type: AttributeType.STRING},
+            billingMode: BillingMode.PAY_PER_REQUEST
+        });
 
-    // Functions
-    const newOrderFunction = new Function(this, 'NewOrderFunction', {
-      runtime: Runtime.NODEJS_24_X,
-      handler: 'handler.newOrder',
-      code: Code.fromAsset('lib/functions'),
-      environment: {
-        PENDING_ORDERS_QUEUE_URL: pendingOrdersQueue.queueUrl
-      }
-    });
+        // SQS
+        const pendingOrdersQueue = new Queue(this, 'PendingOrdersQueue', {});
+        const ordersToSendQueue = new Queue(this, 'OrdersToSendQueue', {});
 
-    pendingOrdersQueue.grantSendMessages(newOrderFunction)
+        // Functions
+        const newOrderFunction = new Function(this, 'NewOrderFunction', {
+            runtime: Runtime.NODEJS_24_X,
+            handler: 'handler.newOrder',
+            code: Code.fromAsset('lib/functions'),
+            environment: {
+                PENDING_ORDERS_QUEUE_URL: pendingOrdersQueue.queueUrl,
+                ORDERS_TABLE_NAME: ordersTable.tableName
+            }
+        });
 
-    const getOrderFunction = new Function(this, 'GetOrderFunction', {
-      runtime: Runtime.NODEJS_24_X,
-      handler: 'handler.getOrder',
-      code: Code.fromAsset('lib/functions'),
-    });
+        pendingOrdersQueue.grantSendMessages(newOrderFunction);
+        ordersTable.grantWriteData(newOrderFunction);
 
-    const prepOrderFunction = new Function(this, 'PrepOrderFunction', {
-      runtime: Runtime.NODEJS_24_X,
-      handler: 'handler.prepOrder',
-      code: Code.fromAsset('lib/functions')
-    });
+        const getOrderFunction = new Function(this, 'GetOrderFunction', {
+            runtime: Runtime.NODEJS_24_X,
+            handler: 'handler.getOrder',
+            code: Code.fromAsset('lib/functions'),
+        });
 
-    prepOrderFunction.addEventSource(new SqsEventSource(pendingOrdersQueue, {
-      batchSize: 1
-    }));
+        const prepOrderFunction = new Function(this, 'PrepOrderFunction', {
+            runtime: Runtime.NODEJS_24_X,
+            handler: 'handler.prepOrder',
+            code: Code.fromAsset('lib/functions')
+        });
 
-    const sendOrderFunction = new Function(this, 'SendOrderFunction', {
-      runtime: Runtime.NODEJS_24_X,
-      handler: 'handler.sendOrder',
-      code: Code.fromAsset('lib/functions'),
-      environment: {
-        ORDERS_TO_SEND_QUEUE_URL: ordersToSendQueue.queueUrl
-      }
-    })
+        prepOrderFunction.addEventSource(new SqsEventSource(pendingOrdersQueue, {
+            batchSize: 1
+        }));
 
-    ordersToSendQueue.grantSendMessages(sendOrderFunction)
+        const sendOrderFunction = new Function(this, 'SendOrderFunction', {
+            runtime: Runtime.NODEJS_24_X,
+            handler: 'handler.sendOrder',
+            code: Code.fromAsset('lib/functions'),
+            environment: {
+                ORDERS_TO_SEND_QUEUE_URL: ordersToSendQueue.queueUrl
+            }
+        })
 
-    // API Gateway
-    const api = new RestApi(this, 'PizzaApi', {
-      restApiName: 'Pizza CDK Service'
-    });
+        ordersToSendQueue.grantSendMessages(sendOrderFunction)
 
-    const orderResource = api.root.addResource('order');
-    orderResource.addMethod('POST', new LambdaIntegration(newOrderFunction));
-    orderResource.addResource('{orderId}').addMethod('GET', new LambdaIntegration(getOrderFunction));
+        // API Gateway
+        const api = new RestApi(this, 'PizzaApi', {
+            restApiName: 'Pizza CDK Service'
+        });
 
-    
-  }
+        const orderResource = api.root.addResource('order');
+        orderResource.addMethod('POST', new LambdaIntegration(newOrderFunction));
+        orderResource.addResource('{orderId}').addMethod('GET', new LambdaIntegration(getOrderFunction));
+    }
 }
